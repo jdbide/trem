bafc46fe6d3c8c8ed4f7410b3e9671ad7e53a79e
Gabriel Gagnier <gabriel.gagnier@avancial.com>
2016-05-20 15:27:11 +0200

    Mise au propre des injection sur TremasImportData et TablesMotriceService

--- src/main/java/com/avancial/app/service/TablesMotriceService.java
+++ src/main/java/com/avancial/app/service/TablesMotriceService.java
@@ -2,19 +2,20 @@ package com.avancial.app.service;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 import com.avancial.app.data.model.databean.TablesMotriceDataBean;
+import com.avancial.socle.persistence.qualifiers.Socle_PUSocle;
 
 public class TablesMotriceService {
-   // TODO @Inject
+
+   @Inject
+   @Socle_PUSocle
    private EntityManager em;
 
    public List<TablesMotriceDataBean> getAllTablesMotrice() {
-      // FIXME a cenlever lors de la mise en place de l'@Inject
-      this.em = Persistence.createEntityManagerFactory("PU_app").createEntityManager();
 
       Query query = this.em.createNamedQuery("TablesMortice.getAll", TablesMotriceDataBean.class);
       return query.getResultList();
