381b1022e8c7232041fc5808f14fea54f6f31efa
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-05-24 09:25:51 +0200

    [TREM-68] Service import des tables : utilisation injection

--- src/main/java/com/avancial/app/service/TablesImportService.java
+++ src/main/java/com/avancial/app/service/TablesImportService.java
@@ -2,55 +2,60 @@ package com.avancial.app.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
 import com.avancial.socle.persistence.qualifiers.Socle_PUSocle;
+
 /**
- * Service permetant de gerer les tables d'import
+ * Service permettant de gérer les tables d'import
+ * 
  * @author gabriel.gagnier
  *
  */
 public class TablesImportService {
-   @Inject
-   @Socle_PUSocle
-   private EntityManager em;
-   
-/**
- * renvoi les données contenue dans la table entityName (exemple: "TMDAVTR") en JSON
- * @param entityName
- * @return
- * @throws ClassNotFoundException
- */
-   public JSONObject getDataTable(String entityName) throws ClassNotFoundException {
-      GetEntiteService entiteService = new GetEntiteService();
-      
-      Class<?> entity = null;
+    @Inject
+    @Socle_PUSocle
+    private EntityManager em;
+    
+    @Inject
+    private GetEntiteService entiteService;
 
-      try {
-         entity = entiteService.getClasseEntiteImporFromTableMotrice(entityName);
-      } catch (ClassNotFoundException e) {
-         e.printStackTrace();
-      }
+    /**
+     * Renvoie les données contenues dans la table entityName (exemple:
+     * "TMDAVTR") en JSON
+     * 
+     * @param entityName
+     * @return
+     * @throws ClassNotFoundException
+     */
+    public JSONObject getDataTable(String entityName) throws ClassNotFoundException {
+        Class<?> entity = null;
 
-      List<String> colNames = new ArrayList<>();
-      for (int i = 0; i < entity.getDeclaredFields().length; i++) {
-         colNames.add(entity.getDeclaredFields()[i].getName());
-      }
+        try {
+            entity = entiteService.getClasseEntiteImporFromTableMotrice(entityName);
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-      Query query = this.em.createNamedQuery(entiteService.getNomEntiteImportFromNomEntiteMotrice(entityName)+".getAll", entiteService.getClasseEntiteImporFromTableMotrice(entityName));
-      List<Object> tmdavtrDataBeans = query.getResultList();
-      JSONArray datas = new JSONArray();
-      datas.addAll(tmdavtrDataBeans);
+        List<String> colNames = new ArrayList<>();
+        for (int i = 0; i < entity.getDeclaredFields().length; i++) {
+            colNames.add(entity.getDeclaredFields()[i].getName());
+        }
 
-      JSONObject retour = new JSONObject();
-      retour.put("cols", colNames);
-      retour.put("dataset", datas);
-      return retour;
-   }
+        Query query = this.em.createNamedQuery(
+                entiteService.getNomEntiteImportFromNomEntiteMotrice(entityName) + ".getAll",
+                entiteService.getClasseEntiteImporFromTableMotrice(entityName));
+        List<Object> tmdavtrDataBeans = query.getResultList();
+        JSONArray datas = new JSONArray();
+        datas.addAll(tmdavtrDataBeans);
+
+        JSONObject retour = new JSONObject();
+        retour.put("cols", colNames);
+        retour.put("dataset", datas);
+        return retour;
+    }
 }
--- src/main/java/com/avancial/app/service/TablesMotriceService.java
+++ src/main/java/com/avancial/app/service/TablesMotriceService.java
@@ -1,32 +1,32 @@
 package com.avancial.app.service;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
 import com.avancial.app.data.databean.TablesMotriceDataBean;
 import com.avancial.socle.persistence.qualifiers.Socle_PUSocle;
 
 /**
- * Service permetant la gestion de TablesMotrice
+ * Service permettant la gestion de TablesMotrice
+ * 
  * @author gabriel.gagnier
  *
  */
 public class TablesMotriceService {
 
-   @Inject
-   @Socle_PUSocle
-   private EntityManager em;
+    @Inject
+    @Socle_PUSocle
+    private EntityManager em;
 
-   /**
-    * retourne le contenue de la table TablesMotrice
-    * @return List<TablesMotriceDataBean>
-    */
-   public List<TablesMotriceDataBean> getAllTablesMotrice() {
-      Query query = this.em.createNamedQuery("TablesMortice.getAll", TablesMotriceDataBean.class);
-      return query.getResultList();
-   }
+    /**
+     * Retourne le contenu de la table TablesMotrice
+     * 
+     * @return List<{@link TablesMotriceDataBean}>
+     */
+    public List<TablesMotriceDataBean> getAllTablesMotrice() {
+        Query query = this.em.createNamedQuery("TablesMortice.getAll", TablesMotriceDataBean.class);
+        return query.getResultList();
+    }
 
 }
