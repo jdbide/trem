153fd3801b94a20f82ab1166aaa56015ab7cff54
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-05-24 09:30:06 +0200

    [TREM-68] Bean TablesMotrice : enlevé le champs "entity" inutile

--- src/main/java/com/avancial/app/data/databean/TablesMotriceDataBean.java
+++ src/main/java/com/avancial/app/data/databean/TablesMotriceDataBean.java
@@ -8,52 +8,43 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tremas_tables_motrice")
-@NamedQuery(name = "TablesMortice.getAll", query= "SELECT t FROM TablesMotriceDataBean t")
+@Table(name = "tremas_tables_motrice")
+@NamedQuery(name = "TablesMortice.getAll", query = "SELECT t FROM TablesMotriceDataBean t")
 public class TablesMotriceDataBean {
-   
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Id
-   private Long idTablesMotrice;
-   private String libelleTablesMotrice;
-   private String entityTablesMotrice;
-   /**
-    * @return the idTablesMotrice
-    */
-   public Long getIdTablesMotrice() {
-      return this.idTablesMotrice;
-   }
-   /**
-    * @param idTablesMotrice the idTablesMotrice to set
-    */
-   public void setIdTablesMotrice(Long idTablesMotrice) {
-      this.idTablesMotrice = idTablesMotrice;
-   }
-   /**
-    * @return the libelleTablesMotrice
-    */
-   public String getLibelleTablesMotrice() {
-      return this.libelleTablesMotrice;
-   }
-   /**
-    * @param libelleTablesMotrice the libelleTablesMotrice to set
-    */
-   public void setLibelleTablesMotrice(String libelleTablesMotrice) {
-      this.libelleTablesMotrice = libelleTablesMotrice;
-   }
-   /**
-    * @return the entityTablesMotrice
-    */
-   public String getEntityTablesMotrice() {
-      return this.entityTablesMotrice;
-   }
-   /**
-    * @param entityTablesMotrice the entityTablesMotrice to set
-    */
-   public void setEntityTablesMotrice(String entityTablesMotrice) {
-      this.entityTablesMotrice = entityTablesMotrice;
-   }
-   
-   
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long idTablesMotrice;
+    private String libelleTablesMotrice;
+
+    /**
+     * @return the idTablesMotrice
+     */
+    public Long getIdTablesMotrice() {
+        return this.idTablesMotrice;
+    }
+
+    /**
+     * @param idTablesMotrice
+     *            the idTablesMotrice to set
+     */
+    public void setIdTablesMotrice(Long idTablesMotrice) {
+        this.idTablesMotrice = idTablesMotrice;
+    }
+
+    /**
+     * @return the libelleTablesMotrice
+     */
+    public String getLibelleTablesMotrice() {
+        return this.libelleTablesMotrice;
+    }
+
+    /**
+     * @param libelleTablesMotrice
+     *            the libelleTablesMotrice to set
+     */
+    public void setLibelleTablesMotrice(String libelleTablesMotrice) {
+        this.libelleTablesMotrice = libelleTablesMotrice;
+    }
 
 }
