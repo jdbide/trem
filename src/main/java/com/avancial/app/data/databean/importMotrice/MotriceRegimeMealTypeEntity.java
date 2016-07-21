b4b3ebe78d5e9219d5102ba7d774f8fb6f811495
MULLER Mikael <mikael.muller@avancial.com>
2016-07-21 09:25:00 +0200

    [TREM-126] Entité pour RegimeMealType

--- src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeMealTypeEntity.java
+++ src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeMealTypeEntity.java
@@ -1,5 +1,7 @@
 package com.avancial.app.data.databean.importMotrice;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -15,13 +17,56 @@ import javax.persistence.Table;
 @Table(name = "tremas_motrice_regime_mealtype")
 @NamedQuery(name = "MotriceRegimeMealTypeEntity.getAll", query = "SELECT t FROM MotriceRegimeMealTypeEntity t")
 public class MotriceRegimeMealTypeEntity {
-   
+
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Id
-   private Long idMotriceRegimeMealTypeEntity;
-   
-   @OneToOne(fetch=FetchType.LAZY)
-   @JoinColumn(name="idMotriceRegime")
+   private Long                idMotriceRegimeMealTypeEntity;
+   private String              mealTypeMotriceRegimeMealType;
+   private Date                beginServiceHourRegimeMealType;
+   private Date                endServiceHourRegimeMealType;
+
+   @OneToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "idMotriceRegime")
    private MotriceRegimeEntity motriceRegime;
+
+   public Long getIdMotriceRegimeMealTypeEntity() {
+      return idMotriceRegimeMealTypeEntity;
+   }
+
+   public void setIdMotriceRegimeMealTypeEntity(Long idMotriceRegimeMealTypeEntity) {
+      this.idMotriceRegimeMealTypeEntity = idMotriceRegimeMealTypeEntity;
+   }
+
+   public String getMealTypeMotriceRegimeMealType() {
+      return mealTypeMotriceRegimeMealType;
+   }
+
+   public void setMealTypeMotriceRegimeMealType(String mealTypeMotriceRegimeMealType) {
+      this.mealTypeMotriceRegimeMealType = mealTypeMotriceRegimeMealType;
+   }
+
+   public Date getBeginServiceHourRegimeMealType() {
+      return beginServiceHourRegimeMealType;
+   }
+
+   public void setBeginServiceHourRegimeMealType(Date beginServiceHourRegimeMealType) {
+      this.beginServiceHourRegimeMealType = beginServiceHourRegimeMealType;
+   }
+
+   public Date getEndServiceHourRegimeMealType() {
+      return endServiceHourRegimeMealType;
+   }
+
+   public void setEndServiceHourRegimeMealType(Date endServiceHourRegimeMealType) {
+      this.endServiceHourRegimeMealType = endServiceHourRegimeMealType;
+   }
+
+   public MotriceRegimeEntity getMotriceRegime() {
+      return motriceRegime;
+   }
+
+   public void setMotriceRegime(MotriceRegimeEntity motriceRegime) {
+      this.motriceRegime = motriceRegime;
+   }
 
 }
