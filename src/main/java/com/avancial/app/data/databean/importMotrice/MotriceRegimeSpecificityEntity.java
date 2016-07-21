797beaef3f416c06dbebc0439e761094a897d6bd
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-07-21 09:37:51 +0200

    [TREM-119] Entité pour RegimeSpecificity

--- src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeSpecificityEntity.java
+++ src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeSpecificityEntity.java
@@ -1,5 +1,6 @@
 package com.avancial.app.data.databean.importMotrice;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -14,13 +15,69 @@ import javax.persistence.Table;
 @Table(name = "tremas_motrice_regime_specificity")
 @NamedQuery(name = "MotriceRegimeSpecificity.getAll", query = "SELECT t FROM MotriceRegimeSpecificityEntity t")
 public class MotriceRegimeSpecificityEntity {
-   
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   @Id
-   private Long idMotriceRegimeSpecificity;
-   
-   @ManyToOne(fetch=FetchType.LAZY)
-   @JoinColumn(name="idMotriceRegime")
-   private MotriceRegimeEntity motriceRegime;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long idMotriceRegimeSpecificity;
+    @Column(length = 3, nullable = false)
+    private String coachNumber;
+    @Column(length = 3, nullable = false)
+    private String compartmentNumber;
+    @Column(length = 3, nullable = false)
+    private String seatNumber;
+    @Column(length = 2, nullable = false)
+    private String stateCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idMotriceRegime")
+    private MotriceRegimeEntity motriceRegime;
+
+    public Long getIdMotriceRegimeSpecificity() {
+        return this.idMotriceRegimeSpecificity;
+    }
+
+    public void setIdMotriceRegimeSpecificity(Long idMotriceRegimeSpecificity) {
+        this.idMotriceRegimeSpecificity = idMotriceRegimeSpecificity;
+    }
+
+    public String getCoachNumber() {
+        return this.coachNumber;
+    }
+
+    public void setCoachNumber(String coachNumber) {
+        this.coachNumber = coachNumber;
+    }
+
+    public String getCompartmentNumber() {
+        return this.compartmentNumber;
+    }
+
+    public void setCompartmentNumber(String compartmentNumber) {
+        this.compartmentNumber = compartmentNumber;
+    }
+
+    public String getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getStateCode() {
+        return this.stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public MotriceRegimeEntity getMotriceRegime() {
+        return this.motriceRegime;
+    }
+
+    public void setMotriceRegime(MotriceRegimeEntity motriceRegime) {
+        this.motriceRegime = motriceRegime;
+    }
 
 }
