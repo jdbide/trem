6eeb40bea9ba121cf270033907c2a0b682173385
MULLER Mikael <mikael.muller@avancial.com>
2016-07-21 09:26:25 +0200

    [TREM-121] Entité pour RegimeDistribution

--- src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeDistributionEntity.java
+++ src/main/java/com/avancial/app/data/databean/importMotrice/MotriceRegimeDistributionEntity.java
@@ -14,13 +14,38 @@ import javax.persistence.Table;
 @Table(name = "tremas_motrice_regime_distribution")
 @NamedQuery(name = "MotriceRegimeDistribution.getAll", query = "SELECT t FROM MotriceRegimeDistributionEntity t")
 public class MotriceRegimeDistributionEntity {
-   
+
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Id
-   private Long idMotriceRegimeDistribution;
-   
-   @OneToOne(fetch=FetchType.LAZY)
-   @JoinColumn(name="idMotriceRegime")
+   private Long                idMotriceRegimeDistribution;
+   private String              distribIndexMotriceRegimeDistribution;
+
+   @OneToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "idMotriceRegime")
    private MotriceRegimeEntity motriceRegime;
+
+   public Long getIdMotriceRegimeDistribution() {
+      return idMotriceRegimeDistribution;
+   }
+
+   public void setIdMotriceRegimeDistribution(Long idMotriceRegimeDistribution) {
+      this.idMotriceRegimeDistribution = idMotriceRegimeDistribution;
+   }
+
+   public String getDistribIndexMotriceRegimeDistribution() {
+      return distribIndexMotriceRegimeDistribution;
+   }
+
+   public void setDistribIndexMotriceRegimeDistribution(String distribIndexMotriceRegimeDistribution) {
+      this.distribIndexMotriceRegimeDistribution = distribIndexMotriceRegimeDistribution;
+   }
+
+   public MotriceRegimeEntity getMotriceRegime() {
+      return motriceRegime;
+   }
+
+   public void setMotriceRegime(MotriceRegimeEntity motriceRegime) {
+      this.motriceRegime = motriceRegime;
+   }
 
 }
