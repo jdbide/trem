749a8dd233d9c0b3c2159382396f22c80913efec
Bruno LE GLOAHEC <bruno.legloahec@avancial.com>
2016-07-26 14:43:19 +0200

    gestion du webService pour récupérer les paramètres. Récupération du nom de l'application pour la page d'accueil.

--- src/main/webapp/appClient/socle/controllers/globalCtrl.js
+++ src/main/webapp/appClient/socle/controllers/globalCtrl.js
@@ -4,11 +4,21 @@
  * Contrôleur global de l'application, utilisé au niveau
  * de la directive ng-app
  */
-socle_app.controller("globalCtrl", ["$scope", "$rootScope", 'envService', 'userService', 'menuService', 
-                                    function($scope, $rootScope, envService, userService, menuService) {
+socle_app.controller("globalCtrl", ["$scope", "$rootScope", 'envService', 'userService', 'menuService', 'paramService',
+                                    function($scope, $rootScope, envService, userService, menuService,paramService) {
 	function constructor () {
 		$scope.welcome = "Socle 2";
 		console.log(envService.get());
+		
+		// Récupération paramètres
+		paramService.getDataByServer('app','nom_projet').then(function() {
+			$scope.welcome = paramService.getParam();
+		}, function() {
+			alert("Probleme au niveau des paramètres");
+		});	
+	
+		
+		
 		// Récupération des infos de l'utilisateur
 		userService.getDataByServer().then(function() {
 			$scope.user = userService.getUser();
