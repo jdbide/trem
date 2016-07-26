17358dd37f8dea15549b01ffe8cb55af63ed64a2
hamza.laterem <hamza.laterem@avancial.com>
2016-07-26 19:45:55 +0200

    correction d'une ano

--- src/main/webapp/appClient/socle/config/config.route.js
+++ src/main/webapp/appClient/socle/config/config.route.js
@@ -6,8 +6,7 @@
 socle_app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: 'socle/views/partials/accueil.xhtml',
-		controller: 'globalCtrl'
+		templateUrl: 'socle/views/partials/accueil.xhtml'
 	})
 	.when('/pages', {
 		templateUrl: 'socle/views/partials/socleTable.xhtml',
