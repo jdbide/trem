5baad56aa1b3346b32dd44f222c71427dfaac12d
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-07-26 11:01:24 +0200

    [TREM-95] Squelette du traitement pour insérer dans nos tables motrice

--- src/main/java/com/avancial/app/traitement/TraitementImportJeuDonnees.java
+++ src/main/java/com/avancial/app/traitement/TraitementImportJeuDonnees.java
@@ -4,7 +4,6 @@
 package com.avancial.app.traitement;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -93,6 +92,17 @@ public class TraitementImportJeuDonnees extends ATraitementLogDetail {
             this.log("Echec du \"set\" de TranscodageRegimeMotrice.dateDebutService");
             e.printStackTrace();
         }
+        
+        /* Insertion dans les tables du modèle motrice */
+        TraitementMotrice traitementMotrice = new TraitementMotrice(jeuDonneeDataBean);
+        try {
+            traitementMotrice.execute();
+        }
+        catch (SecurityException e) {
+            this.log("Echec du traitement motrice.");
+            e.printStackTrace();
+        }
+
         this.traitementOk = true;
     }
 
