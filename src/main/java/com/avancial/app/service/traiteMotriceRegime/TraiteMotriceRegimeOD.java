de3176dfccbb5dce661250866f5bf1699601feb5
MULLER Mikael <mikael.muller@avancial.com>
2016-07-29 10:27:29 +0200

    [TREM-142] Correctif de la requête

--- src/main/java/com/avancial/app/service/traiteMotriceRegime/TraiteMotriceRegimeOD.java
+++ src/main/java/com/avancial/app/service/traiteMotriceRegime/TraiteMotriceRegimeOD.java
@@ -25,7 +25,7 @@ public class TraiteMotriceRegimeOD implements ITraiteMotriceRegime {
       Long idTrainTranche = motriceTrainTrancheEntity.getIdMotriceTrainTranche();
 
       Query queryRODTranche = entityManager.createNativeQuery(
-            "SELECT tranche.TRCH_INPT_RR_ORIG AS oriMotriceRegimeOD, tranche.TRCH_INPT_RR_DEST AS destMotriceRegimeOD, tranche.TRCH_REGI_VAL AS regime " + "FROM tremas_import_tmdtrch AS tranche " + "INNER JOIN tremas_import_tmdcath AS cath ON cath.CATH_TRCH_COD_CIE = tranche.TRCH_TRA1_COD_CIE "
+            "SELECT tranche.TRCH_INPT_RR_ORIG AS oriMotriceRegimeOD, tranche.TRCH_INPT_RR_DEST AS destMotriceRegimeOD, tranche.TRCH_REGI_VAL AS motriceRegime " + "FROM tremas_import_tmdtrch AS tranche " + "INNER JOIN tremas_import_tmdcath AS cath ON cath.CATH_TRCH_COD_CIE = tranche.TRCH_TRA1_COD_CIE "
                   + "AND cath.CATH_TRCH_IND_FER = tranche.TRCH_TRA1_IND_FER " + "AND cath.CATH_TRCH_NUM_TRA1 = tranche.TRCH_TRA1_NUM_TRA1 " + "AND cath.CATH_TRCH_NUM = tranche.TRCH_NUM " + "WHERE cath.CATH_SSIM = ? " + "AND tranche.TRCH_TRA1_NUM_TRA1 = ? " + "ORDER BY motriceRegime");
       queryRODTranche.setParameter(1, motriceTrainTrancheEntity.getTrancheNumberMotriceTrainTranche());
       queryRODTranche.setParameter(2, motriceTrainTrancheEntity.getTrainNumberMotriceTrainTranche());
