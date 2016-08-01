b6963027f6bcd0687da29596ad9989505b8e0ef0
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-08-01 16:52:33 +0200

    [TREM-152] Implémentation d'objets métier (fin)

--- src/main/java/com/avancial/app/data/objetsMetier/PlanTransport/TypeEquipement.java
+++ src/main/java/com/avancial/app/data/objetsMetier/PlanTransport/TypeEquipement.java
@@ -2,4 +2,24 @@ package com.avancial.app.data.objetsMetier.PlanTransport;
 
 public class TypeEquipement {
 
+    private String typeEquipement;
+
+    private Regime regime;
+
+    public String getTypeEquipement() {
+        return this.typeEquipement;
+    }
+
+    public void setTypeEquipement(String typeEquipement) {
+        this.typeEquipement = typeEquipement;
+    }
+
+    public Regime getRegime() {
+        return this.regime;
+    }
+
+    public void setRegime(Regime regime) {
+        this.regime = regime;
+    }
+
 }
