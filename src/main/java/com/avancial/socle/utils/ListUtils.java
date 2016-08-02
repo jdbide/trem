3d55ca1013eddb148dfe534b514a8a50fc2fd139
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-08-02 16:16:03 +0200

    Template sur la comparaison de deux listes

--- src/main/java/com/avancial/socle/utils/ListUtils.java
+++ src/main/java/com/avancial/socle/utils/ListUtils.java
@@ -13,13 +13,13 @@ public class ListUtils {
      * @param l2
      * @return
      */
-    public static boolean compareLists(List<Object> l1, List<Object> l2) {
-        for (Object object : l1) {
+    public static<T> boolean compareLists(List<T> l1, List<T> l2) {
+        for (T object : l1) {
             if (!l2.contains(object)) {
                 return false;
             }
         }
-        for (Object object : l2) {
+        for (T object : l2) {
             if (!l1.contains(object)) {
                 return false;
             }
