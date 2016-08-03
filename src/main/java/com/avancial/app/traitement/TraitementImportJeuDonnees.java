079a118c577651dad7948497a2b9ffb246a5cc19
hamza.laterem <hamza.laterem@avancial.com>
2016-08-03 16:14:58 +0200

    Mise à jour de la methode traitement

--- src/main/java/com/avancial/app/traitement/TraitementImportJeuDonnees.java
+++ src/main/java/com/avancial/app/traitement/TraitementImportJeuDonnees.java
@@ -7,9 +7,13 @@ import java.util.Date;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+
+import com.avancial.app.data.databean.CompagnieEnvironnementEntity;
 import com.avancial.app.data.databean.JeuDonneeEntity;
-import com.avancial.app.data.dto.ImportJeuDonneesDto;
+import com.avancial.app.data.databean.Status;
+import com.avancial.app.data.dto.ImportTmsDto;
 import com.avancial.app.persistence.EntityManagerFactoryProviderDb2;
+import com.avancial.app.service.CompagnieEnvironnementService;
 import com.avancial.app.service.JeuDonneeService;
 import com.avancial.app.utilitaire.SchemaMotrice;
 import com.avancial.socle.persistence.qualifiers.Socle_PUSocle;
@@ -28,11 +32,13 @@ public class TraitementImportJeuDonnees extends ATraitementLogDetail {
     EntityManager entityManagerDb2;
 
     @Inject
+    private CompagnieEnvironnementService compagnieEnvironnementService;
+    @Inject
     private JeuDonneeService jeuDonneeService;
 
     private boolean traitementOk;
 
-    private ImportJeuDonneesDto importJeuDonneesDto;
+    private ImportTmsDto importTmsDto;
 
     @Inject
     private TraitementMotrice traitementMotrice;
@@ -52,64 +58,68 @@ public class TraitementImportJeuDonnees extends ATraitementLogDetail {
      */
     @Override
     protected void executeTraitement() {
-        try {
-            this.entityManagerDb2 = EntityManagerFactoryProviderDb2
-                    .getInstance(this.importJeuDonneesDto.getUsername(), this.importJeuDonneesDto.getPassword())
-                    .createEntityManager();
-        }
-        catch (Throwable ex) {
-            this.logBean.setExceptionTraitement(ex.getMessage());
-            this.logBean.setMessageTraitement("Echec de connexion avec la base de données externe Db2");
-            throw ex;
-        }
+       CompagnieEnvironnementEntity compagnieEnvironnementEntity = null;
+       JeuDonneeEntity jeuDonneeDataBean = null;
+       try {
+          try {
+             // Récupération de l'environnement sélectionné
+             compagnieEnvironnementEntity = this.compagnieEnvironnementService.getCompagnieEnvironnementById(this.importTmsDto.getIdCompagnieEnvironnement());
+          }
+          catch (Throwable ex) {
+             this.logBean.setExceptionTraitement(ex.getMessage());
+             this.logBean.setMessageTraitement("L'environnement sélectionné n'existe pas");
+             throw ex;
+          }
 
-        JeuDonneeEntity jeuDonneeDataBean = this.initJeuDonnee();
-        jeuDonneeDataBean.getIdJeuDonnees();
+           try {
+              // Instanciation EntityManagerFactory avec les bonnes données de la dataSource de l'environnement
+               this.entityManagerDb2 = EntityManagerFactoryProviderDb2
+                       .getInstance(compagnieEnvironnementEntity, this.importTmsDto.getUsername(), this.importTmsDto.getPassword()).createEntityManager();
+           }
+           catch (Throwable ex) {
+               this.logBean.setExceptionTraitement(ex.getMessage());
+               this.logBean.setMessageTraitement("Echec de connexion avec la base de données externe Db2");
+               throw ex;
+           }
+           
+           // Instanciation et sauvegarde du nouveau jeu de données
+           jeuDonneeDataBean = this.jeuDonneeService.initJeuDonnee(compagnieEnvironnementEntity);
+           this.jeuDonneeService.save(jeuDonneeDataBean);
 
-        // vider puis importer les tables
-        TraitementImportDb2Motrice traitement = new TraitementImportDb2Motrice(this.entityManagerSocle,
-                this.entityManagerDb2, SchemaMotrice.ES.getSchema());
-        try {
-            traitement.execute();
-        }
-        catch (SecurityException e) {
-            this.log("Echec de l'import");
-            e.printStackTrace();
-        }
+           // vider puis importer les tables
+           TraitementImportDb2Motrice traitement = new TraitementImportDb2Motrice(this.entityManagerSocle,
+                   this.entityManagerDb2, SchemaMotrice.ES.getSchema());
+           try {
+               traitement.execute();
+           } catch (SecurityException e) {
+              this.log("Echec de l'import");
+              e.printStackTrace();
+           }
 
-        jeuDonneeDataBean.setDateLastUpdateJeuDonnees(new Date());
-        this.jeuDonneeService.update(jeuDonneeDataBean);
-        
-        /* Insertion dans les tables du modèle motrice */
-        this.traitementMotrice.setJeuDonneeEntity(jeuDonneeDataBean);
-        try {
-            this.traitementMotrice.execute();
-        }
-        catch (Exception e) {
-            this.log("Echec du traitement motrice.");
-            e.printStackTrace();
-            throw e;
-        }
+           /* Insertion dans les tables du modèle motrice */
+           this.traitementMotrice.setJeuDonneeEntity(jeuDonneeDataBean);
+           try {
+               this.traitementMotrice.execute();
+           }
+           catch (Exception e) {              
+               this.log("Echec du traitement motrice.");
+               e.printStackTrace();
+               throw e;
+           }
 
-        this.traitementOk = true;
-    }
-
-    /**
-     * Initialise le jeu de données qui va être sauvegardé.
-     * 
-     * @return
-     */
-    public JeuDonneeEntity initJeuDonnee() {
-        // création de l'entite
-        JeuDonneeEntity jeuDonneeDataBean = new JeuDonneeEntity();
-        jeuDonneeDataBean.setDateCreateJeuDonnees(new Date());
-        jeuDonneeDataBean.setDateLastUpdateJeuDonnees(new Date());
-        jeuDonneeDataBean.setIdUtilisateurCreateJeuDonnees(-1);
-        jeuDonneeDataBean.setIdUtilisateurLastUpdateJeuDonnees(-1);
-        jeuDonneeDataBean.setActifJeuDonnees(true);
-        jeuDonneeDataBean.setCommentaireJeuDonnees("");
-
-        return this.jeuDonneeService.save(jeuDonneeDataBean);
+           this.traitementOk = true;
+           jeuDonneeDataBean.setDateLastUpdateJeuDonnees(new Date());
+           jeuDonneeDataBean.setStatusJeuDonnees(Status.DRAFT);
+       }  catch (Throwable ex) {
+          if (jeuDonneeDataBean != null) {
+             jeuDonneeDataBean.setDateLastUpdateJeuDonnees(new Date());
+             jeuDonneeDataBean.setActifJeuDonnees(false);
+          }
+       } finally {
+          if (jeuDonneeDataBean != null) {
+             this.jeuDonneeService.update(jeuDonneeDataBean);
+          }
+       }
     }
 
     /**
@@ -130,16 +140,16 @@ public class TraitementImportJeuDonnees extends ATraitementLogDetail {
     /**
      * @return the importJeuDonneesDto
      */
-    public ImportJeuDonneesDto getImportJeuDonneesDto() {
-        return this.importJeuDonneesDto;
+    public ImportTmsDto getImportJeuDonneesDto() {
+        return this.importTmsDto;
     }
 
     /**
      * @param importJeuDonneesDto
      *            the importJeuDonneesDto to set
      */
-    public void setImportJeuDonneesDto(ImportJeuDonneesDto importJeuDonneesDto) {
-        this.importJeuDonneesDto = importJeuDonneesDto;
+    public void setImportJeuDonneesDto(ImportTmsDto importTmsDto) {
+        this.importTmsDto = importTmsDto;
     }
 
 }
