619d4fbf8a5afe34c8e62ac0453812f5ad181c18
hamza.laterem <hamza.laterem@avancial.com>
2016-08-03 12:27:28 +0200

    Suppression des fichiers

--- src/main/java/com/avancial/app/data/databean/importMotrice/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contient les Entitées Motrice Avancial
- */
-/**
- * @author gabriel.gagnier
- *
- */
-package com.avancial.app.data.databean.importMotrice;
--- src/main/java/com/avancial/app/data/databean/package-info.java
+++ src/main/java/com/avancial/app/data/databean/package-info.java
@@ -1,10 +1,8 @@
 /**
- * @author hamza.laterem
- * 
- * Elle consiste en la partie gérant l'accès aux données du système. Ces données peuvent être propres au système, 
- * ou gérées par un autre système. 
- * 
- * La couche métier n'a pas à s'adapter à ces deux cas, ils sont transparents pour elle, et elle accède aux données de manière uniforme (couplage faible).
+ * Contient les Entitées Motrice Avancial
+ */
+/**
+ * @author gabriel.gagnier
  *
  */
-package com.avancial.app.data.databean;
+package com.avancial.app.data.databean.importMotrice;
