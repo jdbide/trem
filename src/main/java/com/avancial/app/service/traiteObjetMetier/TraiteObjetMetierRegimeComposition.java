41438ab1e0dfe67870230baea7474fae4c9d01f1
MULLER Mikael <mikael.muller@avancial.com>
2016-08-09 12:12:19 +0200

    [TREM-203] Remplissage du régime composition

--- src/main/java/com/avancial/app/service/traiteObjetMetier/TraiteObjetMetierRegimeComposition.java
+++ src/main/java/com/avancial/app/service/traiteObjetMetier/TraiteObjetMetierRegimeComposition.java
@@ -1,16 +1,39 @@
 package com.avancial.app.service.traiteObjetMetier;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.avancial.app.data.databean.importMotrice.MotriceRegimeCompositionCoachEntity;
+import com.avancial.app.data.databean.importMotrice.MotriceRegimeCompositionEntity;
 import com.avancial.app.data.databean.importMotrice.MotriceRegimeEntity;
+import com.avancial.app.data.databean.importMotrice.MotriceRegimeStopEntity;
+import com.avancial.app.data.objetsMetier.PlanTransport.ASousRegimeTranche;
+import com.avancial.app.data.objetsMetier.PlanTransport.Composition;
+import com.avancial.app.data.objetsMetier.PlanTransport.Desserte;
+import com.avancial.app.data.objetsMetier.PlanTransport.Gare;
+import com.avancial.app.data.objetsMetier.PlanTransport.GareHoraire;
+import com.avancial.app.data.objetsMetier.PlanTransport.Horaire;
+import com.avancial.app.data.objetsMetier.PlanTransport.Regime;
 import com.avancial.app.data.objetsMetier.PlanTransport.Tranche;
+import com.avancial.app.data.objetsMetier.PlanTransport.Voiture;
 
 public class TraiteObjetMetierRegimeComposition implements ITraiteObjetMetier {
 
    @Override
    public void traite(AtomicReference<Tranche> atomicTranche, MotriceRegimeEntity regime) {
-      // TODO Auto-generated method stub
-      
+      List<ASousRegimeTranche> listeCompositions = (List<ASousRegimeTranche>) atomicTranche.get().getAttributsField(Composition.class);
+      if (listeCompositions == null) {
+         listeCompositions = new ArrayList<ASousRegimeTranche>();
+      }
+      for (MotriceRegimeCompositionEntity regimeComposition : regime.getMotriceRegimeComposition()) {
+         List<Voiture> voitures = new ArrayList<Voiture>();
+         for (MotriceRegimeCompositionCoachEntity voiture : regimeComposition.getCarsNumbers()) {
+            voitures.add(new Voiture(voiture.getCoachNumberMotriceRegimeCompositionCoach(), null));
+         }
+         listeCompositions.add(new Composition(regimeComposition.getClassCodeMotriceRegimeComposition(), regimeComposition.getDiagCodeMotriceRegimeComposition(), regimeComposition.getRameCodeMotriceRegimeComposition(), regimeComposition.getRmCodeMotriceRegimeComposition(), voitures, new Regime(regime.getPeriodMotriceRegime())));
+      }   
+      atomicTranche.get().addAttributsField(listeCompositions);
    }
 
 }
