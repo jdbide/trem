adff21b1d80135fd167b9ab1a006ec1da1aeac42
Gabriel Gagnier <gabriel.gagnier@avancial.com>
2016-08-19 12:09:14 +0200

    delete d'un jeu de données (75sec) (nouvelles class)

---
+++ src/main/java/com/avancial/app/traitement/TraiteDeleteRegimeDistribution.java
@@ -0,0 +1,16 @@
+package com.avancial.app.traitement;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import com.avancial.app.data.databean.importMotrice.MotriceRegimeEntity;
+
+public class TraiteDeleteRegimeDistribution implements ITraiteDeleteDonnees {
+
+    @Override
+    public void execute(List<MotriceRegimeEntity> regimes, EntityManager entityManager) {
+        entityManager.createNamedQuery("MotriceRegimeDistribution.deleteByRegimes").setParameter("regimes", regimes)
+                .executeUpdate();
+
+    }
+
+}
---
+++ src/main/java/com/avancial/app/traitement/TraiteDeleteRegimeSatcode.java
@@ -0,0 +1,15 @@
+package com.avancial.app.traitement;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import com.avancial.app.data.databean.importMotrice.MotriceRegimeEntity;
+
+public class TraiteDeleteRegimeSatcode implements ITraiteDeleteDonnees {
+
+    @Override
+    public void execute(List<MotriceRegimeEntity> regimes, EntityManager entityManager) {
+        entityManager.createNamedQuery("MotriceRegimeSatcode.deleteByRegimes").setParameter("regimes", regimes)
+                .executeUpdate();
+    }
+
+}
