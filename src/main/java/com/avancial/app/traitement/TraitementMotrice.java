5c900bd971609a1cf4e6714685d7ef5932044c6c
unknown <sebastien.benede@CZC3137DTH.sysrail.local>
2016-08-22 17:43:10 +0200

    TREM-132 Validation d'un jeu de données

--- src/main/java/com/avancial/app/traitement/TraitementMotrice.java
+++ src/main/java/com/avancial/app/traitement/TraitementMotrice.java
@@ -10,10 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
-import org.hibernate.Session;
 
 import com.avancial.app.data.databean.CompagnieEnvironnementEntity;
 import com.avancial.app.data.databean.JeuDonneeEntity;
@@ -34,7 +31,7 @@ import com.avancial.app.utilitaire.GetEntiteService;
 import com.avancial.app.utilitaire.MapGeneratorTablesMotriceRegime;
 import com.avancial.app.utilitaire.MapIdTablesMotriceRegime;
 import com.avancial.app.utilitaire.MapPlansDeTransport;
-import com.avancial.socle.persistence.qualifiers.Socle_PUSocle;
+import com.avancial.socle.session.Session;
 import com.avancial.socle.traitement.ATraitementLogDetail;
 
 public class TraitementMotrice extends ATraitementLogDetail implements Serializable {
@@ -43,42 +40,44 @@ public class TraitementMotrice extends ATraitementLogDetail implements Serializa
 	*/
 	private static final long serialVersionUID = 1L;
 
-   private JeuDonneeEntity               jeuDonneeEntity;
+	private JeuDonneeEntity jeuDonneeEntity;
 
-   @Inject
-   private RefTablesMotriceRegimeService tablesMotriceRegimeService;
+	@Inject
+	private RefTablesMotriceRegimeService tablesMotriceRegimeService;
 
-   @Inject
-   private TraiteMotriceRegimeFactory    traiteMotriceRegimeFactory;
+	@Inject
+	private TraiteMotriceRegimeFactory traiteMotriceRegimeFactory;
+	
+	@Inject
+	private Session session;
 
-   private MapPlansDeTransport           mapPlansDeTransport;
+	private MapPlansDeTransport mapPlansDeTransport;
 
-
-   @Inject
-   public TraitementMotrice() {
+	@Inject
+	public TraitementMotrice() {
 		super();
 	}
 
 	@Override
 	protected void executeTraitement() throws Exception {
-	   
-	   this.em.clear();
-	   this.log("Debut du traitement de l'import brut");      
-      Query query = this.em.createQuery("SELECT t FROM CompagnieEnvironnementEntity t where t.idCompagnieEnvironnement = 1");
-      CompagnieEnvironnementEntity compagnieEnvironnement = ((CompagnieEnvironnementEntity) query.getSingleResult());
-      
-      this.jeuDonneeEntity = new JeuDonneeEntity();
-      this.jeuDonneeEntity.setDateCreateJeuDonnees(new Date());
-      this.jeuDonneeEntity.setDateLastUpdateJeuDonnees(new Date());
-      this.jeuDonneeEntity.setIdUtilisateurCreateJeuDonnees(-1);
-      this.jeuDonneeEntity.setIdUtilisateurLastUpdateJeuDonnees(-1);
-      this.jeuDonneeEntity.setCompagnieEnvironnement(compagnieEnvironnement);
-      
-      this.em.getTransaction().begin();
-      this.em.persist(this.jeuDonneeEntity);
-      this.em.getTransaction().commit();
 
-	   
+		this.em.clear();
+		this.log("Debut du traitement de l'import brut");
+		Query query = this.em
+				.createQuery("SELECT t FROM CompagnieEnvironnementEntity t where t.idCompagnieEnvironnement = 1");
+		CompagnieEnvironnementEntity compagnieEnvironnement = ((CompagnieEnvironnementEntity) query.getSingleResult());
+
+		this.jeuDonneeEntity = new JeuDonneeEntity();
+		this.jeuDonneeEntity.setDateCreateJeuDonnees(new Date());
+		this.jeuDonneeEntity.setDateLastUpdateJeuDonnees(new Date());
+		this.jeuDonneeEntity.setIdUtilisateurCreateJeuDonnees(this.session.getUser().getIdUser().intValue());
+		this.jeuDonneeEntity.setIdUtilisateurLastUpdateJeuDonnees(this.session.getUser().getIdUser().intValue());
+		this.jeuDonneeEntity.setCompagnieEnvironnement(compagnieEnvironnement);
+
+		this.em.getTransaction().begin();
+		this.em.persist(this.jeuDonneeEntity);
+		this.em.getTransaction().commit();
+
 		if (this.jeuDonneeEntity != null) {
 			/* Liste des données liées à un train-tranche */
 			List<RefTablesMotriceRegimeEntity> motriceRegimeEntities = this.tablesMotriceRegimeService
@@ -102,9 +101,9 @@ public class TraitementMotrice extends ATraitementLogDetail implements Serializa
 			}
 
 			MapGeneratorTablesMotriceRegime mapGeneratorTablesMotriceRegime = new MapGeneratorTablesMotriceRegime(
-					this.em.unwrap(Session.class), 250);
+					this.em.unwrap(org.hibernate.Session.class), 250);
 
-			//this.em.getTransaction().begin();
+			// this.em.getTransaction().begin();
 			/* Récupération des train-tranche */
 			query = this.em.createNativeQuery(
 					"SELECT tranche.TRCH_TRA1_NUM_TRA1 AS trainNumberMotriceTrainTranche, categorie.CATH_SSIM AS trancheNumberMotriceTrainTranche, IF ( train.TRA1_NUM_TRAIN IS NULL, 0, 1 ) AS validForRRMotriceTrainTranche, categorie.CATH_ETAT_TRCH AS trancheStatusMotriceTrainTranche, categorie.CATH_REGI AS regime FROM tremas_import_tmdtrch AS tranche LEFT JOIN tremas_import_tmdtra1 AS train ON tranche.TRCH_TRA1_COD_CIE = train.TRA1_CIES_COD_CIE AND tranche.TRCH_TRA1_NUM_TRA1 = train.TRA1_NUM_TRAIN AND tranche.TRCH_TRA1_IND_FER = train.TRA1_IND_FER_ROUTE INNER JOIN tremas_import_tmdcath AS categorie ON tranche.TRCH_TRA1_COD_CIE = categorie.CATH_CIRR_COD_CIE AND tranche.TRCH_TRA1_NUM_TRA1 = categorie.CATH_TRCH_NUM_TRA1 AND tranche.TRCH_TRA1_IND_FER = categorie.CATH_TRCH_IND_FER AND tranche.TRCH_NUM = categorie.CATH_TRCH_NUM");
@@ -149,7 +148,6 @@ public class TraitementMotrice extends ATraitementLogDetail implements Serializa
 
 				this.em.persist(motriceRegimeEntity);
 				this.em.getTransaction().commit();
-				
 
 				System.out.println("Insertion dans la table tremas_motrice_regime du regime train-tranche associe");
 				if (!motriceTrainTrancheEntity.getTrainNumberMotriceTrainTranche().equals(lastTrainNumber)) {
@@ -224,7 +222,7 @@ public class TraitementMotrice extends ATraitementLogDetail implements Serializa
 			this.log("Fin d'insertion dans la table tremas_motrice_regime_composition_coach");
 			this.em.getTransaction().commit();
 			this.em.close();
-			this.log("Fin du traitement de l'import brut"); 
+			this.log("Fin du traitement de l'import brut");
 		}
 
 	}
@@ -244,12 +242,13 @@ public class TraitementMotrice extends ATraitementLogDetail implements Serializa
 			MapGeneratorTablesMotriceRegime mapGeneratorTablesMotriceRegime) throws Exception {
 		IMultipleInsertRequestGenerator multipleInsertRequestGenerator = mapGeneratorTablesMotriceRegime.get(entity);
 		try {
-//			System.out.println(multipleInsertRequestGenerator.getRequest());
-			multipleInsertRequestGenerator.executeRequest();		
+			// System.out.println(multipleInsertRequestGenerator.getRequest());
+			multipleInsertRequestGenerator.executeRequest();
 		} catch (Exception e) {
 			System.err.println(
 					"Erreur dans l'insertion dans la table motrice régime pour l'entité : " + entity.getName());
-			System.out.println("Erreur dans l'insertion dans la table motrice régime pour l'entité : " + entity.getName());
+			System.out.println(
+					"Erreur dans l'insertion dans la table motrice régime pour l'entité : " + entity.getName());
 			e.printStackTrace();
 			throw e;
 		}
