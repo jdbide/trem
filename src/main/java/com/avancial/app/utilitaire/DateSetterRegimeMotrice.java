c926cdfdaefdb58d9f04bcac6d01e984c04b49f8
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-09-20 17:26:32 +0200

    [TREM-231] Mantis #5400 : correction traitement DateSetter dans import brut

--- src/main/java/com/avancial/app/utilitaire/DateSetterRegimeMotrice.java
+++ src/main/java/com/avancial/app/utilitaire/DateSetterRegimeMotrice.java
@@ -1,32 +1,33 @@
 package com.avancial.app.utilitaire;
 
 import java.text.ParseException;
+import java.sql.Date;
+
 /**
  * set la date permetant le transcodage
+ * 
  * @author gabriel.gagnier
  *
  */
 public class DateSetterRegimeMotrice implements ITraitementDonnees {
 
-    private static String app = null;
-    final String appValid = "KHT";
+   private static String app      = null;
+   final String          appValid = "KHT";
 
-    @Override
-    public String execute(Object donnee) throws ParseException {
-        if(app.equals(this.appValid) && donnee instanceof String) {
-        	TranscodageRegimeMotrice.setDateDebutService((String) donnee);
-    		return (String) donnee;
-    	}
-    	
-    	return null;
-    }
+   @Override
+   public String execute(Object donnee) throws ParseException {
+      if (app.equals(this.appValid) && donnee.getClass().equals(Date.class)) {
+         TranscodageRegimeMotrice.setDateDebutService(donnee.toString());
+      }
+      return donnee.toString();
+   }
 
-    /**
-     * @param app
-     *            the app to set
-     */
-    public static void setApp(String app) {
-        DateSetterRegimeMotrice.app = app;
-    }
+   /**
+    * @param app
+    *           the app to set
+    */
+   public static void setApp(String app) {
+      DateSetterRegimeMotrice.app = app;
+   }
 
 }
