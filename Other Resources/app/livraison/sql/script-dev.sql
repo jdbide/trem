d4bbe5655e38d7783844b0f70ae49af4686ae06a
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-10-06 10:32:41 +0200

    Mise à jour script refonte base pour les données de référence

--- Other Resources/app/livraison/sql/script-dev.sql
+++ Other Resources/app/livraison/sql/script-dev.sql
@@ -27,6 +27,15 @@ CREATE TABLE `tremas_jeu_donnees_control` (
 -- ---------------------------------------
 
 DROP TABLE IF EXISTS `tremas_compagnie`;
+DROP TABLE IF EXISTS `tremas_environnement`;
+
+ALTER TABLE `tremas_compagnie_environnement`
+DROP COLUMN `libelleCompagnie`,
+DROP COLUMN `libelleEnvironnement`,
+ADD COLUMN `idCompagnie`  int(11) NULL AFTER `idCompagnieEnvironnement`,
+ADD COLUMN `idEnvironnement`  int(11) NULL AFTER `idCompagnie`,
+DROP INDEX `indImportDonnees_libelleCompagnie_libelleEnvironnement`;
+
 CREATE TABLE `tremas_compagnie` (
   `idCompagnie` int(11) NOT NULL AUTO_INCREMENT,
   `libelleCompagnie` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -38,7 +47,6 @@ CREATE TABLE `tremas_compagnie` (
 INSERT INTO `tremas_compagnie` (`libelleCompagnie`, `nomTechniqueCompagnie`) VALUES ('Eurostar', 'ES');
 INSERT INTO `tremas_compagnie` (`libelleCompagnie`, `nomTechniqueCompagnie`) VALUES ('Thalys', 'TH');
 
-DROP TABLE IF EXISTS `tremas_environnement`;
 CREATE TABLE `tremas_environnement` (
   `idEnvironnement` int(11) NOT NULL AUTO_INCREMENT,
   `libelleEnvironnement` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -50,32 +58,15 @@ CREATE TABLE `tremas_environnement` (
 INSERT INTO `tremas_environnement` (`libelleEnvironnement`, `nomTechniqueEnvironnement`) VALUES ('Recette', 'REC');
 INSERT INTO `tremas_environnement` (`libelleEnvironnement`, `nomTechniqueEnvironnement`) VALUES ('Production', 'PROD');
 
-DROP TABLE IF EXISTS `tremas_compagnie_environnement`;
-CREATE TABLE `tremas_compagnie_environnement` (
-  `idCompagnieEnvironnement` int(11) NOT NULL AUTO_INCREMENT,
-  `idCompagnie` int(11),
-  `idEnvironnement` int(11),
-  `nomTechniqueCompagnieEnvironnement` varchar(50) NOT NULL,
-  `actifCompagnieEnvironnement` tinyint(1) NOT NULL,
-  `ordreCompagnieEnvironnement` int(11) NOT NULL,
-  `commentaireCompagnieEnvironnement` text NOT NULL,
-  `idUtilisateurCreateCompagnieEnvironnement` int(11) NOT NULL,
-  `dateCreateCompagnieEnvironnement` datetime NOT NULL,
-  `idUtilisateurLastUpdateCompagnieEnvironnement` int(11) NOT NULL,
-  `dateLastUpdateCompagnieEnvironnement` datetime NOT NULL,
-  `idDataSource` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idCompagnieEnvironnement`),
-  UNIQUE KEY `indImportDonnees_nomTechnique` (`nomTechniqueCompagnieEnvironnement`) USING BTREE,
-  UNIQUE KEY `indImportDonnees_idCompagnie_idEnvironnement` (`idCompagnie`,`idEnvironnement`) USING BTREE,
-  KEY `tremas_compagnie_environnement_fkDatasource` (`idDataSource`),
-  CONSTRAINT `tremas_compagnie_environnement_fkDatasource` FOREIGN KEY (`idDataSource`) REFERENCES `tremas_datasource` (`idDatasource`) ON DELETE SET NULL ON UPDATE SET NULL,
-  CONSTRAINT `compagnie_environnement_FKidCompagnie` FOREIGN KEY (`idCompagnie`) REFERENCES `tremas_compagnie` (`idCompagnie`) ON DELETE SET NULL ON UPDATE SET NULL,
-  CONSTRAINT `compagnie_environnement_FKidEnvironnement` FOREIGN KEY (`idEnvironnement`) REFERENCES `tremas_environnement` (`idEnvironnement`) ON DELETE SET NULL ON UPDATE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-INSERT INTO `tremas_compagnie_environnement` VALUES (1, 1, 2, 'ES_PROD', 1, 0, ' ', 1, NOW(), 1, NOW(), 1);
-INSERT INTO `tremas_compagnie_environnement` VALUES (2, 2, 2, 'TH_PROD', 1, 1, ' ', 1, NOW(), 1, NOW(), 3);
-INSERT INTO `tremas_compagnie_environnement` VALUES (3, 1, 1, 'ES_REC', 1, 2, ' ', 1, NOW(), 1, NOW(), 2);
-INSERT INTO `tremas_compagnie_environnement` VALUES (4, 2, 1, 'TH_REC', 1, 3, ' ', 1, NOW(), 1, NOW(), 4);
+ALTER TABLE `tremas_compagnie_environnement`
+ADD UNIQUE KEY `indImportDonnees_idCompagnie_idEnvironnement` (`idCompagnie`,`idEnvironnement`) USING BTREE,
+ADD CONSTRAINT `compagnie_environnement_FKidCompagnie` FOREIGN KEY (`idCompagnie`) REFERENCES `tremas_compagnie` (`idCompagnie`) ON DELETE SET NULL ON UPDATE SET NULL,
+ADD CONSTRAINT `compagnie_environnement_FKidEnvironnement` FOREIGN KEY (`idEnvironnement`) REFERENCES `tremas_environnement` (`idEnvironnement`) ON DELETE SET NULL ON UPDATE SET NULL;
+
+UPDATE `tremas_compagnie_environnement` SET `idCompagnie`='1', `idEnvironnement`='2' WHERE (`idCompagnieEnvironnement`='1');
+UPDATE `tremas_compagnie_environnement` SET `idCompagnie`='2', `idEnvironnement`='2' WHERE (`idCompagnieEnvironnement`='2');
+UPDATE `tremas_compagnie_environnement` SET `idCompagnie`='1', `idEnvironnement`='1' WHERE (`idCompagnieEnvironnement`='3');
+UPDATE `tremas_compagnie_environnement` SET `idCompagnie`='2', `idEnvironnement`='1' WHERE (`idCompagnieEnvironnement`='4');
 
 -- ---------------------------------------
 -- Tables de référence + Tables motrice regime
