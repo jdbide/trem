3af3c0c5dd45c186699242484623ab0cc08f86d8
hamza.laterem <hamza.laterem@avancial.com>
2016-10-10 19:06:44 +0200

    Implementation de la page search

---
+++ src/main/webapp/appClient/app/controllers/trainManagerSysteme/search/searchTmsCtrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Contrôleur qui gère la page search du chapitre "Train manager systeme / search", 
+ *
+ */
+socle_app.controller("searchTmsCtrl", ['$rootScope', '$scope', function($rootScope, $scope) {
+	$scope.
+	
+	function constructor () {
+		
+	}
+	
+	constructor ();
+}]);
