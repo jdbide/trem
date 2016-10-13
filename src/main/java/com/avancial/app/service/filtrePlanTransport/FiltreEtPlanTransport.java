4cee445605eb24f2657ce2f718ad46dc9f37f7fd
heloise.guillemaud <heloise.guillemaud@avancial.com>
2016-10-13 15:14:47 +0200

    [TREM-249] Nombre indéterminé de filtres en paramètre pour les liaisons logiques "ou" et "et"

--- src/main/java/com/avancial/app/service/filtrePlanTransport/FiltreEtPlanTransport.java
+++ src/main/java/com/avancial/app/service/filtrePlanTransport/FiltreEtPlanTransport.java
@@ -3,14 +3,19 @@ package com.avancial.app.service.filtrePlanTransport;
 import com.avancial.app.data.objetsMetier.PlanTransport.PlanTransport;
 
 /**
- * Filtre les données d'un plan de transport en appliquant deux filtres à la suite.
+ * Filtre les données d'un plan de transport en appliquant plusieurs filtres à la suite.
  * 
  * @author heloise.guillemaud
  *
  */
 public class FiltreEtPlanTransport extends AFiltreEtPlanTransport {
 
-   public FiltreEtPlanTransport(IFiltre<PlanTransport> filtreUn, IFiltre<PlanTransport> filtreDeux) {
-      super(filtreUn, filtreDeux);
+   /**
+    * 
+    * @param filtres
+    *           Liste des filtres à appliquer à la suite
+    */
+   public FiltreEtPlanTransport(IFiltre<PlanTransport>... filtres) {
+      super(filtres);
    }
 }
--- src/main/java/com/avancial/app/service/filtrePlanTransport/FiltreEtRegimePlanTransport.java
+++ src/main/java/com/avancial/app/service/filtrePlanTransport/FiltreEtRegimePlanTransport.java
@@ -3,15 +3,20 @@ package com.avancial.app.service.filtrePlanTransport;
 import com.avancial.app.data.objetsMetier.PlanTransport.PlanTransport;
 
 /**
- * Filtre les régimes tranche d'un plan de transport en appliquant deux filtres à la suite.
+ * Filtre les régimes tranche d'un plan de transport en appliquant plusieurs filtres à la suite.
  * 
  * @author heloise.guillemaud
  *
  */
 public class FiltreEtRegimePlanTransport extends AFiltreEtPlanTransport {
 
-   public FiltreEtRegimePlanTransport(IFiltre<PlanTransport> filtreUn, IFiltre<PlanTransport> filtreDeux) {
-      super(filtreUn, filtreDeux);
+   /**
+    * 
+    * @param filtres
+    *           Liste des filtres à appliquer à la suite
+    */
+   public FiltreEtRegimePlanTransport(IFiltre<PlanTransport>... filtres) {
+      super(filtres);
    }
 
 }
