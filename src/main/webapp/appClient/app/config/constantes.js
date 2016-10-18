719cb04527d9fbbb7b4c13ddaba9c47f5aa75b78
BIDE Jean Daniel <jeandaniel.bide@avancial.com>
2016-10-18 17:17:51 +0200

    Début affichage page search

--- src/main/webapp/appClient/app/config/constantes.js
+++ src/main/webapp/appClient/app/config/constantes.js
@@ -4,6 +4,18 @@
  * App : Gestion de la navigation
  */
 socle_app
+.constant("ClassSousRegime", {
+	"Desserte" : "class com.avancial.app.data.objetsMetier.PlanTransport.Desserte",
+	"ServiceABord" : "class com.avancial.app.data.objetsMetier.PlanTransport.ServiceABord",
+	"Tosp" : "class com.avancial.app.data.objetsMetier.PlanTransport.Tosp",
+	"Repas" : "class com.avancial.app.data.objetsMetier.PlanTransport.Repas",
+	"Restriction" : "class com.avancial.app.data.objetsMetier.PlanTransport.Restriction",
+	"OrigineDestination" : "class com.avancial.app.data.objetsMetier.PlanTransport.OrigineDestination",
+	"Distribution" : "class com.avancial.app.data.objetsMetier.PlanTransport.Distribution",
+	"TypeEquipement" : "class com.avancial.app.data.objetsMetier.PlanTransport.TypeEquipement",
+	"Composition" : "class com.avancial.app.data.objetsMetier.PlanTransport.Composition",
+	"CodeSat" : "class com.avancial.app.data.objetsMetier.PlanTransport.CodeSat"
+})
 .constant("Status", {
 	"IMPORT" : "IMPORT",
 	"DRAFT" : "DRAFT",
--- src/main/webapp/appClient/app/controllers/trainManagerSysteme/search/resultTmsCtrl.js
+++ src/main/webapp/appClient/app/controllers/trainManagerSysteme/search/resultTmsCtrl.js
@@ -4,9 +4,10 @@
  * Contrôleur qui gère la page search du chapitre "Train manager systeme / search", 
  *
  */
-socle_app.controller("resultTmsCtrl", ['$rootScope', '$scope', 'loadingService', '$q', 'searchService', 'resultTmsService',
-                                       function($rootScope, $scope, loadingService, $q, searchService, resultTmsService) {
-	$scope.datas = null;
+socle_app.controller("resultTmsCtrl", ['$rootScope', '$scope', 'loadingService', '$q', 'resultService', 'searchService', 'resultTmsService',
+                                       function($rootScope, $scope, loadingService, $q, resultService, searchService, resultTmsService) {
+	$scope.datas = resultTmsService.getReponse().data;
+	$scope.tab = resultService.getTbody();	
 	
 	
 	function constructor() {
@@ -18,7 +19,26 @@ socle_app.controller("resultTmsCtrl", ['$rootScope', '$scope', 'loadingService',
 					console.warn("==> Récupération des resultat de la recherche <==");
 					var reponse = resultTmsService.getReponse();
 					if (reponse.status) {
-						$scope.datas = reponse.data;
+						$scope.datas = reponse.data;		
+						
+						//On boucle sur les trains du plan de transport
+						angular.forEach(datas.trains, function(train, key) {	  
+							currentTrain = train.numeroTrain;
+							//On boucle sur les tranches du train
+							angular.forEach(train.tranches, function(tranche, key) {	  
+							currentTranche = tranche.numeroTranche;
+								//On boucle sur la liste de jours du régime
+								angular.forEach(tranche.regime.listejours, function(jour, key) {	
+									if(tranche.attributs.)
+									
+								});
+								
+							});
+							
+						});
+						
+						
+						
 						console.log($scope.datas);
 					} else {
 						alert("Erreur : " + reponse.message);
@@ -36,11 +56,13 @@ socle_app.controller("resultTmsCtrl", ['$rootScope', '$scope', 'loadingService',
 	$scope.newSearch = function () {
 		searchService.initNewSearch();
 		$scope.datas = null;
+		resultTmsService.initReponse();
 		$rootScope.changePage();
 	}
 	
 	$scope.modifSearch = function () {
 		$scope.datas = null;
+		resultTmsService.initReponse();
 		$rootScope.isModif = true;
 		$rootScope.changePage();
 	}
