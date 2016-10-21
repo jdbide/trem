af4ec3486a8a6dd88c9cda60fd0132aa0029273c
Gabriel Gagnier <gabriel.gagnier@avancial.com>
2016-10-21 18:18:35 +0200

    [TREM-258] test resultat filtre

--- src/main/java/com/avancial/app/data/dto/filtrePlanTransport/MealServiceDto.java
+++ src/main/java/com/avancial/app/data/dto/filtrePlanTransport/MealServiceDto.java
@@ -10,101 +10,179 @@ import java.util.Date;
  *
  */
 public class MealServiceDto {
-   private String numeroCoach;
-   private String mealType;
-   private String mealCode;
-   private String type;
-   private Date starting;
-   private Date  ending;
-   
-   /**
-    * constructor vide
-    */
-   public MealServiceDto() {
-      super();
-   }
+    private String numeroCoach;
+    private String mealType;
+    private String mealCode;
+    private String type;
+    private Date starting;
+    private Date ending;
 
-   /**
-    * @return the numeroCoach
-    */
-   public String getNumeroCoach() {
-      return numeroCoach;
-   }
+    /**
+     * constructor vide
+     */
+    public MealServiceDto() {
+        super();
+    }
 
-   /**
-    * @param numeroCoach the numeroCoach to set
-    */
-   public void setNumeroCoach(String numeroCoach) {
-      this.numeroCoach = numeroCoach;
-   }
+    /**
+     * @return the numeroCoach
+     */
+    public String getNumeroCoach() {
+        return this.numeroCoach;
+    }
 
-   /**
-    * @return the mealType
-    */
-   public String getMealType() {
-      return mealType;
-   }
+    /**
+     * @param numeroCoach
+     *            the numeroCoach to set
+     */
+    public void setNumeroCoach(String numeroCoach) {
+        this.numeroCoach = numeroCoach;
+    }
 
-   /**
-    * @param mealType the mealType to set
-    */
-   public void setMealType(String mealType) {
-      this.mealType = mealType;
-   }
+    /**
+     * @return the mealType
+     */
+    public String getMealType() {
+        return this.mealType;
+    }
 
-   /**
-    * @return the mealCode
-    */
-   public String getMealCode() {
-      return mealCode;
-   }
+    /**
+     * @param mealType
+     *            the mealType to set
+     */
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
 
-   /**
-    * @param mealCode the mealCode to set
-    */
-   public void setMealCode(String mealCode) {
-      this.mealCode = mealCode;
-   }
+    /**
+     * @return the mealCode
+     */
+    public String getMealCode() {
+        return this.mealCode;
+    }
 
-   /**
-    * @return the type
-    */
-   public String getType() {
-      return type;
-   }
+    /**
+     * @param mealCode
+     *            the mealCode to set
+     */
+    public void setMealCode(String mealCode) {
+        this.mealCode = mealCode;
+    }
 
-   /**
-    * @param type the type to set
-    */
-   public void setType(String type) {
-      this.type = type;
-   }
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return this.type;
+    }
 
-   /**
-    * @return the starting
-    */
-   public Date getStarting() {
-      return starting;
-   }
+    /**
+     * @param type
+     *            the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
 
-   /**
-    * @param starting the starting to set
-    */
-   public void setStarting(Date starting) {
-      this.starting = starting;
-   }
+    /**
+     * @return the starting
+     */
+    public Date getStarting() {
+        return this.starting;
+    }
 
-   /**
-    * @return the ending
-    */
-   public Date getEnding() {
-      return ending;
-   }
+    /**
+     * @param starting
+     *            the starting to set
+     */
+    public void setStarting(Date starting) {
+        this.starting = starting;
+    }
 
-   /**
-    * @param ending the ending to set
-    */
-   public void setEnding(Date ending) {
-      this.ending = ending;
-   }
+    /**
+     * @return the ending
+     */
+    public Date getEnding() {
+        return this.ending;
+    }
+
+    /**
+     * @param ending
+     *            the ending to set
+     */
+    public void setEnding(Date ending) {
+        this.ending = ending;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.ending == null) ? 0 : this.ending.hashCode());
+        result = prime * result + ((this.mealCode == null) ? 0 : this.mealCode.hashCode());
+        result = prime * result + ((this.mealType == null) ? 0 : this.mealType.hashCode());
+        result = prime * result + ((this.numeroCoach == null) ? 0 : this.numeroCoach.hashCode());
+        result = prime * result + ((this.starting == null) ? 0 : this.starting.hashCode());
+        result = prime * result + ((this.type == null) ? 0 : this.type.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MealServiceDto other = (MealServiceDto) obj;
+        if (this.ending == null) {
+            if (other.ending != null)
+                return false;
+        }
+        else if (!this.ending.equals(other.ending))
+            return false;
+        if (this.mealCode == null) {
+            if (other.mealCode != null)
+                return false;
+        }
+        else if (!this.mealCode.equals(other.mealCode))
+            return false;
+        if (this.mealType == null) {
+            if (other.mealType != null)
+                return false;
+        }
+        else if (!this.mealType.equals(other.mealType))
+            return false;
+        if (this.numeroCoach == null) {
+            if (other.numeroCoach != null)
+                return false;
+        }
+        else if (!this.numeroCoach.equals(other.numeroCoach))
+            return false;
+        if (this.starting == null) {
+            if (other.starting != null)
+                return false;
+        }
+        else if (!this.starting.equals(other.starting))
+            return false;
+        if (this.type == null) {
+            if (other.type != null)
+                return false;
+        }
+        else if (!this.type.equals(other.type))
+            return false;
+        return true;
+    }
+
 }
--- src/main/java/com/avancial/app/data/dto/filtrePlanTransport/StopsDto.java
+++ src/main/java/com/avancial/app/data/dto/filtrePlanTransport/StopsDto.java
@@ -10,105 +10,176 @@ import java.util.Date;
  *
  */
 public class StopsDto {
-   private String cityCode;
-   private String libelle;
-   private Date   arrivalTime;
-   private Date   DepartureTime;
-   private String offForbidden;
-   private String onForbidden;
+    private String cityCode;
+    private String libelle;
+    private Date arrivalTime;
+    private Date DepartureTime;
+    private String offForbidden;
+    private String onForbidden;
 
-   public StopsDto() {
-      super();
-   }
+    public StopsDto() {
+        super();
+    }
 
-   /**
-    * @return the cityCode
-    */
-   public String getCityCode() {
-      return cityCode;
-   }
+    /**
+     * @return the cityCode
+     */
+    public String getCityCode() {
+        return this.cityCode;
+    }
 
-   /**
-    * @param cityCode
-    *           the cityCode to set
-    */
-   public void setCityCode(String cityCode) {
-      this.cityCode = cityCode;
-   }
+    /**
+     * @param cityCode
+     *            the cityCode to set
+     */
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
 
-   /**
-    * @return the libelle
-    */
-   public String getLibelle() {
-      return libelle;
-   }
+    /**
+     * @return the libelle
+     */
+    public String getLibelle() {
+        return this.libelle;
+    }
 
-   /**
-    * @param libelle
-    *           the libelle to set
-    */
-   public void setLibelle(String libelle) {
-      this.libelle = libelle;
-   }
+    /**
+     * @param libelle
+     *            the libelle to set
+     */
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
 
-   /**
-    * @return the arrivalTime
-    */
-   public Date getArrivalTime() {
-      return arrivalTime;
-   }
+    /**
+     * @return the arrivalTime
+     */
+    public Date getArrivalTime() {
+        return this.arrivalTime;
+    }
 
-   /**
-    * @param arrivalTime
-    *           the arrivalTime to set
-    */
-   public void setArrivalTime(Date arrivalTime) {
-      this.arrivalTime = arrivalTime;
-   }
+    /**
+     * @param arrivalTime
+     *            the arrivalTime to set
+     */
+    public void setArrivalTime(Date arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
 
-   /**
-    * @return the departureTime
-    */
-   public Date getDepartureTime() {
-      return DepartureTime;
-   }
+    /**
+     * @return the departureTime
+     */
+    public Date getDepartureTime() {
+        return this.DepartureTime;
+    }
 
-   /**
-    * @param departureTime
-    *           the departureTime to set
-    */
-   public void setDepartureTime(Date departureTime) {
-      DepartureTime = departureTime;
-   }
+    /**
+     * @param departureTime
+     *            the departureTime to set
+     */
+    public void setDepartureTime(Date departureTime) {
+        this.DepartureTime = departureTime;
+    }
 
-   /**
-    * @return the offForbidden
-    */
-   public String getOffForbidden() {
-      return offForbidden;
-   }
+    /**
+     * @return the offForbidden
+     */
+    public String getOffForbidden() {
+        return this.offForbidden;
+    }
 
-   /**
-    * @param offForbidden
-    *           the offForbidden to set
-    */
-   public void setOffForbidden(String offForbidden) {
-      this.offForbidden = offForbidden;
-   }
+    /**
+     * @param offForbidden
+     *            the offForbidden to set
+     */
+    public void setOffForbidden(String offForbidden) {
+        this.offForbidden = offForbidden;
+    }
 
-   /**
-    * @return the onForbidden
-    */
-   public String getOnForbidden() {
-      return onForbidden;
-   }
+    /**
+     * @return the onForbidden
+     */
+    public String getOnForbidden() {
+        return this.onForbidden;
+    }
 
-   /**
-    * @param onForbidden
-    *           the onForbidden to set
-    */
-   public void setOnForbidden(String onForbidden) {
-      this.onForbidden = onForbidden;
-   }
+    /**
+     * @param onForbidden
+     *            the onForbidden to set
+     */
+    public void setOnForbidden(String onForbidden) {
+        this.onForbidden = onForbidden;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.DepartureTime == null) ? 0 : this.DepartureTime.hashCode());
+        result = prime * result + ((this.arrivalTime == null) ? 0 : this.arrivalTime.hashCode());
+        result = prime * result + ((this.cityCode == null) ? 0 : this.cityCode.hashCode());
+        result = prime * result + ((this.libelle == null) ? 0 : this.libelle.hashCode());
+        result = prime * result + ((this.offForbidden == null) ? 0 : this.offForbidden.hashCode());
+        result = prime * result + ((this.onForbidden == null) ? 0 : this.onForbidden.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StopsDto other = (StopsDto) obj;
+        if (this.DepartureTime == null) {
+            if (other.DepartureTime != null)
+                return false;
+        }
+        else if (!this.DepartureTime.equals(other.DepartureTime))
+            return false;
+        if (this.arrivalTime == null) {
+            if (other.arrivalTime != null)
+                return false;
+        }
+        else if (!this.arrivalTime.equals(other.arrivalTime))
+            return false;
+        if (this.cityCode == null) {
+            if (other.cityCode != null)
+                return false;
+        }
+        else if (!this.cityCode.equals(other.cityCode))
+            return false;
+        if (this.libelle == null) {
+            if (other.libelle != null)
+                return false;
+        }
+        else if (!this.libelle.equals(other.libelle))
+            return false;
+        if (this.offForbidden == null) {
+            if (other.offForbidden != null)
+                return false;
+        }
+        else if (!this.offForbidden.equals(other.offForbidden))
+            return false;
+        if (this.onForbidden == null) {
+            if (other.onForbidden != null)
+                return false;
+        }
+        else if (!this.onForbidden.equals(other.onForbidden))
+            return false;
+        return true;
+    }
 
 }
